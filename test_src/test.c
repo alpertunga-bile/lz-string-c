4b2fed8ab0241776c2285ed7b0d994d061dd2571
alpertunga-bile <holy_celestine@hotmail.com>
2024-12-16 17:00:31 +0300

    decompress tests are added

--- test_src/test.c
+++ test_src/test.c
@@ -1,65 +1,7 @@
 #define DEFINE_RKTEST_IMPLEMENTATION
 #include "rktest.h"
 
-#include "../src/lz_string.h"
-
 #include "utility.h"
-
-#include "c_string.h"
-
-int
-base64_compress_result(const char* data_filepath, const char* result_filepath)
-{
-  string_t input;
-  string_init(&input);
-
-  read_data(data_filepath, &input);
-
-  string_t compressed;
-  string_init(&compressed);
-
-  lz_compress_base64(&input, &compressed);
-
-  string_t result;
-  string_init(&result);
-
-  read_data(result_filepath, &result);
-
-  int cmp_result = string_compare_string(&result, &compressed);
-
-  string_free(&input);
-  string_free(&compressed);
-  string_free(&result);
-
-  return cmp_result;
-}
-
-int
-base64_decompress_result(const char* data_filepath)
-{
-  string_t input;
-  string_init(&input);
-
-  read_data(data_filepath, &input);
-
-  string_t compressed;
-  string_init(&compressed);
-
-  lz_compress_base64(&input, &compressed);
-
-  string_t result;
-  string_init(&result);
-
-  lz_decompress_base64(&compressed, &result);
-
-  int cmp_result = string_compare_string(&result, &input);
-
-  string_free(&input);
-  string_free(&compressed);
-  string_free(&result);
-
-  return cmp_result;
-}
 
 TEST(compress_base64, all_ascii)
 {
@@ -106,6 +48,16 @@ TEST(compress_base64, tattoo)
 TEST(decompress_base64, all_ascii)
 {
   EXPECT_EQ(base64_decompress_result("test_src/data/all_ascii/data.bin"), 0);
+}
+
+TEST(decompress_base64, hello_world)
+{
+  EXPECT_EQ(base64_decompress_result("test_src/data/hello_world/data.bin"), 0);
+}
+
+TEST(decompress_base64, lorem_ipsum)
+{
+  EXPECT_EQ(base64_decompress_result("test_src/data/lorem_ipsum/data.bin"), 0);
 }
 
 int
